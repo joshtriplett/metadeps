4008affa2df99f348c5341c51e91d2a98b153803
Simonas Kazlauskas <git@kazlauskas.me>
2020-01-16 16:06:08 +0200

    Modernize crate deps
    * Upgrade toml to 0.5
    * Replace error_chain with anyhow for modern error handling

--- tests/test.rs
+++ tests/test.rs
@@ -10,10 +10,18 @@ lazy_static! {
     static ref LOCK: Mutex<()> = Mutex::new(());
 }
 
-fn toml(path: &str) -> metadeps::Result<std::collections::HashMap<String, pkg_config::Library>> {
+fn toml(
+    path: &str,
+) -> Result<std::collections::HashMap<String, pkg_config::Library>, metadeps::Error> {
     let _l = LOCK.lock();
-    env::set_var("PKG_CONFIG_PATH", &env::current_dir().unwrap().join("tests"));
-    env::set_var("CARGO_MANIFEST_DIR", &env::current_dir().unwrap().join("tests").join(path));
+    env::set_var(
+        "PKG_CONFIG_PATH",
+        &env::current_dir().unwrap().join("tests"),
+    );
+    env::set_var(
+        "CARGO_MANIFEST_DIR",
+        &env::current_dir().unwrap().join("tests").join(path),
+    );
     env::set_var("CARGO_FEATURE_TEST_FEATURE", "");
     metadeps::probe()
 }
@@ -30,8 +38,11 @@ fn good() {
 
 fn toml_err(path: &str, err_starts_with: &str) {
     let err = toml(path).unwrap_err();
-    if !err.description().starts_with(err_starts_with) {
-        panic!("Expected error to start with: {:?}\nGot error: {:?}", err_starts_with, err);
+    if !format!("{}", err).starts_with(err_starts_with) {
+        panic!(
+            "Expected error to start with: {:?}\nGot error: {:?}",
+            err_starts_with, err
+        );
     }
 }
 
@@ -42,35 +53,53 @@ fn missing_file() {
 
 #[test]
 fn missing_key() {
-    toml_err("toml-missing-key", "No package.metadata.pkg-config in");
+    toml_err("toml-missing-key", "No package.metadata.pkg-config key in");
 }
 
 #[test]
 fn not_table() {
-    toml_err("toml-not-table", "package.metadata.pkg-config not a table in");
+    toml_err(
+        "toml-not-table",
+        "package.metadata.pkg-config not a table in",
+    );
 }
 
 #[test]
 fn version_missing() {
-    toml_err("toml-version-missing", "No version in package.metadata.pkg-config.testlib");
+    toml_err(
+        "toml-version-missing",
+        "No version in package.metadata.pkg-config.testlib",
+    );
 }
 
 #[test]
 fn version_not_string() {
-    toml_err("toml-version-not-string", "package.metadata.pkg-config.testlib not a string or table");
+    toml_err(
+        "toml-version-not-string",
+        "package.metadata.pkg-config.testlib not a string or table",
+    );
 }
 
 #[test]
 fn version_in_table_not_string() {
-    toml_err("toml-version-in-table-not-string", "Unexpected key package.metadata.pkg-config.testlib.version type integer");
+    toml_err(
+        "toml-version-in-table-not-string",
+        "Unexpected key package.metadata.pkg-config.testlib.version type integer",
+    );
 }
 
 #[test]
 fn feature_not_string() {
-    toml_err("toml-feature-not-string", "Unexpected key package.metadata.pkg-config.testlib.feature type integer");
+    toml_err(
+        "toml-feature-not-string",
+        "Unexpected key package.metadata.pkg-config.testlib.feature type integer",
+    );
 }
 
 #[test]
 fn unexpected_key() {
-    toml_err("toml-unexpected-key", "Unexpected key package.metadata.pkg-config.testlib.color type string");
+    toml_err(
+        "toml-unexpected-key",
+        "Unexpected key package.metadata.pkg-config.testlib.color type string",
+    );
 }
