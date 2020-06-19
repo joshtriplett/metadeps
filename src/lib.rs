bc4b1e5da888b0c95cdf0c870fadc1508d869ffd
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-06-19 14:53:52 +0200

    doc: sync with README

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,37 @@
-//! system-deps lets you write `pkg-config` dependencies in `Cargo.toml` metadata,
-//! rather than programmatically in `build.rs`.  This makes those dependencies
+//!`system-deps` lets you write system dependencies in `Cargo.toml` metadata,
+//! rather than programmatically in `build.rs`. This makes those dependencies
 //! declarative, so other tools can read them as well.
-//!
-//! system-deps parses metadata like this in `Cargo.toml`:
+//! Then, to declare a dependency on `testlib >= 1.2`, a conditional dependency
+//! on `testdata >= 4.5` and a dependency on `glib-2.0 >= 2.64`
+//! add the following section:
 //!
 //! ```toml
 //! [package.system-deps]
 //! testlib = "1.2"
-//! testdata = { version = "4.5", feature = "some-feature" }
+//! testdata = { version = "4.5", feature = "use-testdata" }
 //! glib = { name = "glib-2.0", version = "2.64" }
+//! ```
+//!
+//! In your `build.rs`, add:
+//!
+//! ```should_panic
+//! fn main() {
+//!     system_deps::Config::new().probe().unwrap();
+//! }
+//! ```
+//!
+//! Dependency versions can also be controlled using features:
+//!
+//! ```toml
+//! [features]
+//! v1_2 = []
+//! v1_4 = ["v1_4"]
+//!
+//! [package.system-deps]
 //! gstreamer = { name = "gstreamer-1.0", version = "1.0", feature-versions = { v1_2 = "1.2", v1_4 = "1.4" }}
 //! ```
+//!
+//! In this case the highest version among enabled features will be used.
 
 #![deny(missing_docs)]
 
